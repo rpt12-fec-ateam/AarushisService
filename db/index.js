0e8bef9a82d69939647daae73626651e1824af34
aarushis.s.90@gmail.com <aarushis.s.s90@gmail.com>
2019-03-20 20:48:38 -0700

    working on seed

--- db/index.js
+++ db/index.js
@@ -1,11 +1,47 @@
-const db = require('./index.js');
+
 const mysql = require('mysql');
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
 });
 
-connection.connect();
+connection.connect(error => {
+  if (error) {
+    console.log(err);
+  } else {
+    console.log('connected')
+  }
+});
 
-module.exports = connection;
+connection.query('CREATE DATABASE IF NOT EXISTS fec', (error, results) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(results);
+  }
+});
+
+connection.query('USE reviews', (error, results) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(results);
+  }
+});
+
+
+connection.query('CREATE TABLE IF NOT EXISTS reviews')
+
+
+
+const stars = [1,2,3,4,5];
+const userFirstNames = ['Oliver', 'Harry', 'Jack', 'James', 'Noah', 'Charlie', 'Jacob', 'Alfie', 'Fred', 'Logan', 'Olivia', 'Amelia', 'Emily', 'Ava', 'Lily', 'Mia', 'Angie', 'Ella', 'Connie', 'Britney', 'Christina', 'Justin', 'Beyonce', 'Alice', 'Shirley', 'Harper', 'Willow', 'Rose', 'Matilda', 'Hermione', 'Debbie', 'Sarah', 'Hanna', 'Melissa', 'Ashley', 'Jessica'];
+const userLastInitials = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const headers = ['Yummy', 'Gross', 'Delicious', 'Zesty', 'Sweet', 'Sour', 'Spicy', 'Refreshing', 'Healthy and Tasty', 'Too Healthy', 'Tasty', 'Weird'];
+const dates = [];
+const review = [];
+const tips = [];
+
+const randomReview = {};
+
--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,12 @@
-const connection = require('./index.js');
+// const connection = require('./index.js');
 
-const stars = [1,2,3,4,5];
-const userFirstNames = ['Oliver', 'Harry', 'Jack', 'James', 'Noah', 'Charlie', 'Jacob', 'Alfie', 'Fred', 'Logan', 'Olivia', 'Amelia', 'Emily', 'Ava', 'Lily', 'Mia', 'Angie', 'Ella', 'Connie', 'Britney', 'Christina', 'Justin', 'Beyonce', 'Alice', 'Shirley', 'Harper', 'Willow', 'Rose', 'Matilda', 'Hermione', 'Debbie', 'Sarah', 'Hanna', 'Melissa', 'Ashley', 'Jessica'];
-const userLastInitials = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const headers = ['Yummy', 'Gross', 'Delicious', 'Zesty', 'Sweet', 'Sour', 'Spicy', 'Refreshing', 'Healthy and Tasty', 'Too Healthy', 'Tasty', 'Weird'];
-const dates = [];
-const review = [];
-const tips = [];
+// const stars = [1,2,3,4,5];
+// const userFirstNames = ['Oliver', 'Harry', 'Jack', 'James', 'Noah', 'Charlie', 'Jacob', 'Alfie', 'Fred', 'Logan', 'Olivia', 'Amelia', 'Emily', 'Ava', 'Lily', 'Mia', 'Angie', 'Ella', 'Connie', 'Britney', 'Christina', 'Justin', 'Beyonce', 'Alice', 'Shirley', 'Harper', 'Willow', 'Rose', 'Matilda', 'Hermione', 'Debbie', 'Sarah', 'Hanna', 'Melissa', 'Ashley', 'Jessica'];
+// const userLastInitials = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+// const headers = ['Yummy', 'Gross', 'Delicious', 'Zesty', 'Sweet', 'Sour', 'Spicy', 'Refreshing', 'Healthy and Tasty', 'Too Healthy', 'Tasty', 'Weird'];
+// const dates = [];
+// const review = [];
+// const tips = [];
 
-var randomReview = {};
+// var randomReview = {};
 
